44b56fa835770c4e8fc5ddfa00c382e510fa9db0
RyanLee716 <lee2830@mydusd.org>
2024-10-09 13:48:22 -0700

    Update ReviewRunner.java

--- ReviewRunner.java
+++ ReviewRunner.java
@@ -1,9 +1,13 @@
 class ReviewRunner {
   public static void main(String[] args) 
   {
+    // Make array of the words in the file (with punctuation)
     String reviewText = Review.textToString("SimpleReview.txt");
     String[] textArr = reviewText.split(" ");
     int index = 0;
+
+    // iterate through each word, look for ones with asteriks and replace
+    // positive adjectives are replaced with positive ones and vice versa
     for (String word : textArr) {
       if ("*".equals(word.substring(0, 1))) {
         if (Review.sentimentVal(textArr[index]) > 0) {
@@ -17,7 +21,7 @@ class ReviewRunner {
       }
       index += 1;
     }
-
+    // print the fake review
     System.out.println(String.join(" ", textArr));
   }
 }
